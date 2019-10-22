9f2dbd20222c497cb00bd1c3538b146c84a1cfea
arasmus8 <arasmus8@gmail.com>
2019-10-22 17:17:39 -0600

    Update Catalyze to use potions

--- src/main/java/potionbrewer/cards/Catalyze.java
+++ src/main/java/potionbrewer/cards/Catalyze.java
@@ -1,15 +1,16 @@
 package potionbrewer.cards;
 
-import basemod.BaseMod;
 import basemod.abstracts.CustomCard;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.potions.EnergyPotion;
+import com.megacrit.cardcrawl.potions.SpeedPotion;
 import potionbrewer.PotionbrewerMod;
+import potionbrewer.actions.UseTempPotionAction;
 import potionbrewer.characters.Potionbrewer;
-import potionbrewer.potions.tonics.FireTonic;
+import potionbrewer.potions.tonics.EnergyTonic;
+import potionbrewer.potions.tonics.SpeedTonic;
 
 import static com.megacrit.cardcrawl.core.CardCrawlGame.languagePack;
 import static potionbrewer.PotionbrewerMod.makeCardPath;
@@ -33,17 +34,11 @@ public class Catalyze extends CustomCard {
     public static final CardColor COLOR = Potionbrewer.Enums.COLOR_CYAN;
 
     private static final int COST = 0;
-
-    private static final int MAGIC = 1;
-    private static final int UPGRADED_MAGIC = 1;
-
 // /STAT DECLARATION/
 
 
     public Catalyze() {
         super(ID, CARD_STRINGS.NAME, IMG, COST, CARD_STRINGS.DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        baseMagicNumber = MAGIC;
-        magicNumber = baseMagicNumber;
         exhaust = true;
     }
 
@@ -51,9 +46,13 @@ public class Catalyze extends CustomCard {
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new GainEnergyAction(magicNumber));
-        this.addToBot(new DrawCardAction(p, magicNumber));
-        BaseMod.publishPostPotionUse(new FireTonic());
+        if (upgraded) {
+            this.addToBot(new UseTempPotionAction(new EnergyPotion(), m));
+            this.addToBot(new UseTempPotionAction(new SpeedPotion(), m));
+        } else {
+            this.addToBot(new UseTempPotionAction(new EnergyTonic(), m));
+            this.addToBot(new UseTempPotionAction(new SpeedTonic(), m));
+        }
     }
 
 
@@ -62,7 +61,6 @@ public class Catalyze extends CustomCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADED_MAGIC);
             rawDescription = CARD_STRINGS.UPGRADE_DESCRIPTION;
             initializeDescription();
         }
