dba3e7dede7d4e94cb0b6f9675bbd0ef97997aa1
arasmus8 <arasmus8@gmail.com>
2019-11-19 13:35:40 -0700

    Fix - Prototype card rewards respect Toxic/Molten Egg.

--- src/main/java/potionbrewer/patches/abstractDungeon/getRewardCards.java
+++ src/main/java/potionbrewer/patches/abstractDungeon/getRewardCards.java
@@ -3,6 +3,8 @@ package potionbrewer.patches.abstractDungeon;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.MoltenEgg2;
+import com.megacrit.cardcrawl.relics.ToxicEgg2;
 import potionbrewer.PotionbrewerMod;
 import potionbrewer.cards.Prototype;
 import potionbrewer.orbs.Reagent;
@@ -19,7 +21,13 @@ public class getRewardCards {
             Reagent a = PotionbrewerMod.reagents.get(0);
             Reagent b = PotionbrewerMod.reagents.get(1);
             Reagent c = PotionbrewerMod.reagents.get(2);
-            __return.add(new Prototype(a, b, c));
+            AbstractCard card = new Prototype(a, b, c);
+            if (card.type == AbstractCard.CardType.SKILL && AbstractDungeon.player.hasRelic(ToxicEgg2.ID)) {
+                card.upgrade();
+            } else if (card.type == AbstractCard.CardType.ATTACK && AbstractDungeon.player.hasRelic(MoltenEgg2.ID)) {
+                card.upgrade();
+            }
+            __return.add(card);
         }
         return __return;
     }
