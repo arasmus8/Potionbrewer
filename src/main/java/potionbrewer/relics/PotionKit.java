511469f885c4c4d51aa9d5c7630602432d458faa
arasmus8 <arasmus8@gmail.com>
2019-11-21 17:13:48 -0700

    Update - Include Tonic description in tip box.

--- src/main/java/potionbrewer/relics/PotionKit.java
+++ src/main/java/potionbrewer/relics/PotionKit.java
@@ -4,7 +4,10 @@ import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.actions.common.ObtainPotionAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.localization.KeywordStrings;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
 import com.megacrit.cardcrawl.potions.PotionSlot;
 import potionbrewer.PotionbrewerMod;
@@ -20,9 +23,15 @@ public class PotionKit extends CustomRelic {
 
     private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("PotionKit.png"));
     private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("PotionKit.png"));
+
+    private static final KeywordStrings KEYWORD_STRINGS = CardCrawlGame.languagePack.getKeywordString("potionbrewer:tonic");
     
     public PotionKit() {
         super(ID, IMG, OUTLINE, RelicTier.STARTER, LandingSound.CLINK);
+        tips.clear();
+        tips.add(new PowerTip(this.name, this.description));
+        tips.add(new PowerTip("Tonic", "A smaller version of a regular potion that can't be found in shops or rewards."));
+        initializeTips();
     }
     
     @Override
