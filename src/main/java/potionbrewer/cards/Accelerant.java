05577b5a21d181f543823f4e927bfd34839e0b48
arasmus8 <arasmus8@gmail.com>
2019-12-02 12:56:20 -0700

    Fix (cosmetic): Card art

--- src/main/java/potionbrewer/cards/Accelerant.java
+++ src/main/java/potionbrewer/cards/Accelerant.java
@@ -19,7 +19,7 @@ public class Accelerant extends CustomCard {
 // TEXT DECLARATION
 
     public static final String ID = PotionbrewerMod.makeID(Accelerant.class.getSimpleName());
-    public static final String IMG = makeCardPath("Skill.png");
+    public static final String IMG = makeCardPath("Accelerant.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
 // Must have an image with the same NAME as the card in your image folder!
 
