a51f283f1224bf14c3c4e339278f86ea6a19f681
arasmus8 <arasmus8@gmail.com>
2019-12-03 11:19:59 -0700

    Fix (cosmetic): Card art

--- src/main/java/potionbrewer/cards/QuickStep.java
+++ src/main/java/potionbrewer/cards/QuickStep.java
@@ -19,7 +19,7 @@ public class QuickStep extends CustomCard {
 // TEXT DECLARATION
 
     public static final String ID = PotionbrewerMod.makeID(QuickStep.class.getSimpleName());
-    public static final String IMG = makeCardPath("Skill.png");
+    public static final String IMG = makeCardPath("QuickStep.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
 // Must have an image with the same NAME as the card in your image folder!
 
