96653a7aa1be79b4fb8a02ee5cce59a6d4f5a935
arasmus8 <arasmus8@gmail.com>
2019-12-03 16:42:41 -0700

    Fix (cosmetic): Card art

--- src/main/java/potionbrewer/cards/TimeBomb.java
+++ src/main/java/potionbrewer/cards/TimeBomb.java
@@ -16,7 +16,7 @@ public class TimeBomb extends CustomCard {
     // TEXT DECLARATION
 
     public static final String ID = PotionbrewerMod.makeID(TimeBomb.class.getSimpleName());
-    public static final String IMG = makeCardPath("Attack.png");
+    public static final String IMG = makeCardPath("TimeBomb.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
     // Must have an image with the same NAME as the card in your image folder!.
 
