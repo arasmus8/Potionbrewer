0e852bf445ab02bd57313b64d838adb51cfc2b6b
arasmus8 <arasmus8@gmail.com>
2019-12-06 13:37:02 -0700

    Fix (cosmetic): Card Art

--- src/main/java/potionbrewer/cards/Spark.java
+++ src/main/java/potionbrewer/cards/Spark.java
@@ -26,7 +26,7 @@ public class Spark extends CustomCard {
 // TEXT DECLARATION
 
     public static final String ID = PotionbrewerMod.makeID(Spark.class.getSimpleName());
-    public static final String IMG = makeCardPath("Attack.png");
+    public static final String IMG = makeCardPath("Spark.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
 // Must have an image with the same NAME as the card in your image folder!.
 
