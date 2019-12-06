42a2f2a0ba2e6362619cb4e5e4bbeb6483c09072
arasmus8 <arasmus8@gmail.com>
2019-12-06 11:55:10 -0700

    Fix (cosmetic): Card Art

--- src/main/java/potionbrewer/cards/IronHeart.java
+++ src/main/java/potionbrewer/cards/IronHeart.java
@@ -15,7 +15,7 @@ import static potionbrewer.PotionbrewerMod.makeCardPath;
 public class IronHeart extends CustomCard {
 
     public static final String ID = PotionbrewerMod.makeID(IronHeart.class.getSimpleName());
-    public static final String IMG = makeCardPath("Power.png");
+    public static final String IMG = makeCardPath("IronHeart.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
