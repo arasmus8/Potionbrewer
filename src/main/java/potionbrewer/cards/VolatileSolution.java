2f8cb534c17786d77f0b80cf28bd42491433c5b0
arasmus8 <arasmus8@gmail.com>
2019-12-06 17:24:50 -0700

    Fix (cosmetic): Card Art

--- src/main/java/potionbrewer/cards/VolatileSolution.java
+++ src/main/java/potionbrewer/cards/VolatileSolution.java
@@ -19,7 +19,7 @@ public class VolatileSolution extends CustomCard {
     // TEXT DECLARATION
 
     public static final String ID = PotionbrewerMod.makeID(VolatileSolution.class.getSimpleName());
-    public static final String IMG = makeCardPath("Attack.png");
+    public static final String IMG = makeCardPath("VolatileSolution.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
     // Must have an image with the same NAME as the card in your image folder!.
 
