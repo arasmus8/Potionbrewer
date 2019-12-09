7645aa19a00061a39f0e19a9035813313cba6070
arasmus8 <arasmus8@gmail.com>
2019-12-09 13:55:13 -0700

    Fix (cosmetic): Card Art

--- src/main/java/potionbrewer/cards/UnstableFormula.java
+++ src/main/java/potionbrewer/cards/UnstableFormula.java
@@ -19,7 +19,7 @@ public class UnstableFormula extends CustomCard {
     // TEXT DECLARATION
 
     public static final String ID = PotionbrewerMod.makeID(UnstableFormula.class.getSimpleName());
-    public static final String IMG = makeCardPath("Attack.png");
+    public static final String IMG = makeCardPath("UnstableFormula.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
     // Must have an image with the same NAME as the card in your image folder!.
 
