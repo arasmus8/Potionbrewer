a3ce0f3cac3d665f6f7d5eda88ee88702507db76
arasmus8 <arasmus8@gmail.com>
2019-12-09 16:39:32 -0700

    Fix (cosmetic): Card Art

--- src/main/java/potionbrewer/cards/Biohazard.java
+++ src/main/java/potionbrewer/cards/Biohazard.java
@@ -17,7 +17,7 @@ public class Biohazard extends CustomCard {
     // TEXT DECLARATION
 
     public static final String ID = PotionbrewerMod.makeID(Biohazard.class.getSimpleName());
-    public static final String IMG = makeCardPath("Skill.png");
+    public static final String IMG = makeCardPath("Biohazard.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
     // Must have an image with the same NAME as the card in your image folder!
 
