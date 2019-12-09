abc1ec5ded94d66397b4e9c0c7710c1d8164cd4c
arasmus8 <arasmus8@gmail.com>
2019-12-09 16:43:10 -0700

    Fix (cosmetic): Card Art

--- src/main/java/potionbrewer/cards/CausticBarrier.java
+++ src/main/java/potionbrewer/cards/CausticBarrier.java
@@ -17,7 +17,7 @@ public class CausticBarrier extends CustomCard {
     // TEXT DECLARATION
 
     public static final String ID = PotionbrewerMod.makeID(CausticBarrier.class.getSimpleName());
-    public static final String IMG = makeCardPath("Skill.png");
+    public static final String IMG = makeCardPath("CausticBarrier.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
     // Must have an image with the same NAME as the card in your image folder!
 
