5018e251cd5524846aa47752127ee4bf80cf1158
arasmus8 <arasmus8@gmail.com>
2019-12-11 16:59:10 -0700

    Fix (cosmetic): Card Art

--- src/main/java/potionbrewer/cards/IronFlesh.java
+++ src/main/java/potionbrewer/cards/IronFlesh.java
@@ -18,7 +18,7 @@ public class IronFlesh extends CustomCard {
 // TEXT DECLARATION
 
     public static final String ID = PotionbrewerMod.makeID(IronFlesh.class.getSimpleName());
-    public static final String IMG = makeCardPath("Skill.png");
+    public static final String IMG = makeCardPath("IronFlesh.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
 // Must have an image with the same NAME as the card in your image folder!
 
