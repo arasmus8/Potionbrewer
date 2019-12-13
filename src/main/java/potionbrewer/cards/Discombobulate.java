1e71dc7168aa84ca648766f75afe677762236712
arasmus8 <arasmus8@gmail.com>
2019-12-13 10:54:33 -0700

    Fix (cosmetic): Card Art

--- src/main/java/potionbrewer/cards/Discombobulate.java
+++ src/main/java/potionbrewer/cards/Discombobulate.java
@@ -18,7 +18,7 @@ public class Discombobulate extends CatalyzeCard {
 // TEXT DECLARATION
 
     public static final String ID = PotionbrewerMod.makeID(Discombobulate.class.getSimpleName());
-    public static final String IMG = makeCardPath("Attack.png");
+    public static final String IMG = makeCardPath("Discombobulate.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
 // Must have an image with the same NAME as the card in your image folder!.
 
