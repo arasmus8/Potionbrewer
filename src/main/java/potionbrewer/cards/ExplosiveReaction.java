3bf98c7ebf1050b94f4cb7a85ba8d41cdb936d88
arasmus8 <arasmus8@gmail.com>
2019-12-13 13:21:27 -0700

    Fix (cosmetic): Card Art

--- src/main/java/potionbrewer/cards/ExplosiveReaction.java
+++ src/main/java/potionbrewer/cards/ExplosiveReaction.java
@@ -19,7 +19,7 @@ public class ExplosiveReaction extends CustomCard {
 // TEXT DECLARATION
 
     public static final String ID = PotionbrewerMod.makeID(ExplosiveReaction.class.getSimpleName());
-    public static final String IMG = makeCardPath("Attack.png");
+    public static final String IMG = makeCardPath("ExplosiveReaction.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
 // Must have an image with the same NAME as the card in your image folder!.
 
