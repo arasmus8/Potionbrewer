0a8e741d1b91ffc7cbfdab0c34ecdc98d310e1a0
arasmus8 <arasmus8@gmail.com>
2019-12-13 19:57:41 -0700

    Fix (cosmetic): Card Art

--- src/main/java/potionbrewer/cards/PortableLab.java
+++ src/main/java/potionbrewer/cards/PortableLab.java
@@ -16,7 +16,7 @@ import static potionbrewer.PotionbrewerMod.makeCardPath;
 public class PortableLab extends CustomCard {
 
     public static final String ID = PotionbrewerMod.makeID(PortableLab.class.getSimpleName());
-    public static final String IMG = makeCardPath("Power.png");
+    public static final String IMG = makeCardPath("PortableLab.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
