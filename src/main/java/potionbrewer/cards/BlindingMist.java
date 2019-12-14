3e75fea977661c982d3778e071c5b3eea65b0e76
arasmus8 <arasmus8@gmail.com>
2019-12-14 08:40:25 -0700

    Fix (cosmetic): Card Art

--- src/main/java/potionbrewer/cards/BlindingMist.java
+++ src/main/java/potionbrewer/cards/BlindingMist.java
@@ -17,7 +17,7 @@ public class BlindingMist extends CustomCard {
     // TEXT DECLARATION
 
     public static final String ID = PotionbrewerMod.makeID(BlindingMist.class.getSimpleName());
-    public static final String IMG = makeCardPath("Skill.png");
+    public static final String IMG = makeCardPath("BlindingMist.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
     // Must have an image with the same NAME as the card in your image folder!
 
