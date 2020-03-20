e01fd18b0d5104cc808d55a4c3bd3f0574021e40
arasmus8 <arasmus8@gmail.com>
2020-03-20 11:02:31 -0600

    fix(balance): rework clay to do big damage on later turns

--- src/main/java/potionbrewer/orbs/Clay.java
+++ src/main/java/potionbrewer/orbs/Clay.java
@@ -1,15 +1,19 @@
 package potionbrewer.orbs;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.localization.OrbStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.orbs.AbstractOrb;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
-import com.megacrit.cardcrawl.powers.TheBombPower;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import potionbrewer.PotionbrewerMod;
 import potionbrewer.potions.FreezingPotion;
 
@@ -21,7 +25,25 @@ public class Clay extends Reagent {
 
     public Clay() {
         super(ORB_ID, img, orbString.NAME, DESC);
-        targeted = false;
+        damages = true;
+        damage = 3;
+        if (AbstractDungeon.isPlayerInDungeon()
+                && AbstractDungeon.getCurrMapNode() != null
+                && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT
+                && PotionbrewerMod.turnNumber > 4
+        ) {
+            damage = 25;
+        }
+    }
+
+    @Override
+    public void applyPowers() {
+        super.applyPowers();
+        if (PotionbrewerMod.turnNumber > 4) {
+            damage = 25;
+        } else {
+            damage = 1;
+        }
     }
 
     @Override
@@ -40,8 +62,13 @@ public class Clay extends Reagent {
     }
 
     @Override
-    public void doEffects(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new ApplyPowerAction(p, p, new TheBombPower(p, 2, 25), 2));
+    public void doDamage(AbstractPlayer p, AbstractMonster m, DamageInfo info) {
+        this.addToBot(new DamageAction(m, info, AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+    }
+
+    @Override
+    public void doAoeDamage(AbstractPlayer p, int damage) {
+        this.addToBot(new DamageAllEnemiesAction(p, DamageInfo.createDamageMatrix(damage), DamageInfo.DamageType.NORMAL, AbstractGameAction.AttackEffect.BLUNT_HEAVY));
     }
 
     static {
